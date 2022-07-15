1b27cc43d534dcefd87b6ed526c1e25f8ef716a9
Administrator <abhinavkrishna4951@gmail.com>
2022-07-15 12:53:07 +0530

    view course created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"",component:AddCourseComponent},
+{path:"viewcourse",component:ViewCourseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/view-course/view-course.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-course',
+  templateUrl: './view-course.component.html',
+  styleUrls: ['./view-course.component.css']
+})
+export class ViewCourseComponent implements OnInit {
+
+  constructor(private api:ApiService) { 
+    this.api.viewCourse().subscribe(
+      (response)=>{
+        this.data=response
+      })
+
+  }
+
+
+  ngOnInit(): void {
+
+    
+    }
+
+  data:any=[]
+  
+
+}
